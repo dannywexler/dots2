fe67ffb58b73e09bca430c95c0c7cae279e742be
Danny Wexler <dannywexler@gmail.com>
2023-03-18 23:26:02 -0700

    changed home-manager symlinking to link files within the folder instead of the folder itself

--- boots/src/boots.ts
+++ boots/src/boots.ts
@@ -45,7 +45,7 @@ async function installNixHomeMgr() {
 async function cloneDots() {
     echo('Cloning dots')
     await gitClone('dannywexler', 'dots2', myDots)
-    await $`ln -sf ${myDots}config/home-manager ${HOME}/.config`
+    await $`ln -sf ${myDots}config/home-manager/ ${HOME}/.config/home-manager`
     echo('Installing all home manager packages')
     await $`home-manager switch`
     echo('Dots cloned and home manager packages installed')
